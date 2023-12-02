0b047482ccea0cba620aa93afed308cb5478b188
Jatin Singhal <jatinsinghal@Jatins-MacBook-Air-2.local>
2023-12-03 02:29:28 +0530

    Fixed Game

--- index.js
+++ index.js
@@ -1,90 +1,211 @@
 const canvas = document.getElementById("main");
 const ctx = canvas.getContext("2d");
 ctx.imageSmoothingEnabled = false;
-
+const beamOffset = 20;
+const adjustedBeamWidth = 3;
+const adjustedBeamHeight = 15;
 const shuttle = new Image();
-const questions = [
-  "What are the risk involved in the reconciliation process?",
-  "Another question...",
-  // Add more questions as needed
-];
-let currentQuestionIndex = 0;
 shuttle.src = "./4x/shuttle.png";
+
 const sun = new Image();
 sun.src = "./4x/sun.png";
+console.log(sun.height);
+sun.height = sun.height / 4;
+sun.width = sun.width / 4;
+console.log(sun.width);
+const beamImg = new Image();
+beamImg.src = "./4x/beam.png";
+beamImg.height = 0.15 * beamImg.height;
+beamImg.width = 0.15 * beamImg.width;
 const quesBox = new Image();
 quesBox.src = "./4x/questionBox.png";
+
+const asteroidImage = new Image();
+asteroidImage.src = "./4x/aes1.png";
+
 const startbtn = document.getElementById("startButton");
-const nextbtn = document.getElementById("nextButton");
-const quesText = document.getElementById("ques");
-//Game Function
-function changeQuestion() {
-  if (currentQuestionIndex < questions.length - 1) {
-    currentQuestionIndex++;
-  } else {
-    currentQuestionIndex = 0; // Loop back to the first question
+
+const center = { x: canvas.width / 2, y: canvas.height / 2 };
+console.log(center.x + center.y);
+const asteroidWidth = 50;
+const asteroidHeight = 50;
+const maxAsteroidSpeed = 2;
+const minAsteroidSpeed = 1;
+const beamSpeed = 5;
+const beamWidth = 5;
+const beamHeight = 20;
+
+const asteroids = [];
+const beams = [];
+
+let mouseX = center.x;
+let mouseY = center.y;
+
+function generateAsteroid() {
+  const asteroidY = Math.random() * (canvas.height / 2) + canvas.height / 4;
+  const asteroidSpeed =
+    Math.random() * (maxAsteroidSpeed - minAsteroidSpeed) + minAsteroidSpeed;
+  const newAsteroid = {
+    x: -20,
+    y: asteroidY,
+    speed: -asteroidSpeed,
+    width: asteroidWidth,
+    height: asteroidHeight,
+  };
+  asteroids.push(newAsteroid);
+}
+
+function shootBeam(event) {
+  const mouseX = event.clientX - canvas.getBoundingClientRect().left;
+  const mouseY = event.clientY - canvas.getBoundingClientRect().top;
+
+  const angle = Math.atan2(mouseY - center.y, mouseX - center.x);
+
+  const beam = {
+    x: center.x + 100 * Math.cos(angle),
+
+    y: center.y + 100 * Math.sin(angle),
+
+    angle: angle,
+    speed: beamSpeed,
+    width: beamImg.width,
+    height: beamImg.height,
+  };
+
+  beams.push(beam);
+}
+
+function updateAsteroids() {
+  for (let i = asteroids.length - 1; i >= 0; i--) {
+    const asteroid = asteroids[i];
+    asteroid.x -= asteroid.speed;
+
+    // Implement collision logic with the shuttle if needed
+
+    if (asteroid.x + asteroid.width < 0) {
+      asteroids.splice(i, 1);
+    }
   }
-  drawGame();
+}
+
+function updateBeams() {
+  for (let i = beams.length - 1; i >= 0; i--) {
+    const beam = beams[i];
+    beam.x += beam.speed * Math.cos(beam.angle);
+    beam.y += beam.speed * Math.sin(beam.angle);
+
+    if (beam.x > canvas.width) {
+      beams.splice(i, 1);
+    }
+  }
+}
+
+function checkCollisions() {
+  for (let i = asteroids.length - 1; i >= 0; i--) {
+    const asteroid = asteroids[i];
+
+    for (let j = beams.length - 1; j >= 0; j--) {
+      const beam = beams[j];
+
+      if (
+        beam.x < asteroid.x + asteroid.width &&
+        beam.x + beam.width > asteroid.x &&
+        beam.y < asteroid.y + asteroid.height &&
+        beam.y + beam.height > asteroid.y
+      ) {
+        asteroids.splice(i, 1);
+        beams.splice(j, 1);
+      }
+    }
+  }
+}
+
+function drawAsteroids() {
+  for (const asteroid of asteroids) {
+    ctx.drawImage(
+      asteroidImage,
+      asteroid.x,
+      asteroid.y,
+      asteroid.width,
+      asteroid.height
+    );
+  }
+}
+
+function drawBeams() {
+  for (const beam of beams) {
+    ctx.save();
+    ctx.translate(beam.x, beam.y);
+    ctx.rotate(beam.angle);
+    ctx.drawImage(
+      beamImg,
+      -beam.width / 2,
+      -beam.height / 2,
+      beam.width,
+      beam.height
+    );
+    ctx.restore();
+  }
+}
+
+function drawShuttle() {
+  const distanceX = 100;
+  ctx.save();
+
+  const angle = Math.atan2(mouseY - center.y, mouseX - center.x);
+  ctx.translate(
+    center.x + distanceX * Math.cos(angle),
+    center.y + distanceX * Math.sin(angle)
+  );
+  ctx.rotate(angle);
+
+  const shuttleWidth = 50;
+  const shuttleHeight = 30;
+
+  ctx.drawImage(
+    shuttle,
+    -shuttleWidth / 2,
+    -shuttleHeight / 2,
+    shuttleWidth,
+    shuttleHeight
+  );
+
+  ctx.restore();
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(
+    sun,
+    center.x - sun.width / 2,
+    center.y - sun.height / 2,
+    sun.width,
+    sun.height
+  );
+  ctx.drawImage(quesBox, center.x + 300, center.y - 50, 130, 130);
+  drawAsteroids();
+  drawBeams();
+  drawShuttle();
+}
+
+function gameLoop() {
+  updateAsteroids();
+  updateBeams();
+  checkCollisions();
+  draw();
+  requestAnimationFrame(gameLoop);
 }
 
 function startGame() {
-  const canvas = document.getElementById("main");
-  const ctx = canvas.getContext("2d");
-  ctx.imageSmoothingEnabled = false;
-
-  const shuttle = new Image();
-  shuttle.src = "./4x/shuttle.png";
-
-  const startbtn = document.getElementById("startButton");
   startbtn.style.display = "none";
+  canvas.addEventListener("click", shootBeam);
 
-  const center = { x: canvas.width / 2, y: canvas.height / 2 + 20 }; // Center of the canvas
-
-  shuttle.onload = function () {
-    // Draw the sun at the center with width and height as 200px
-    const sunWidth = 200;
-    const sunHeight = 200;
-    const sunX = center.x - sunWidth / 2;
-    const sunY = center.y - sunHeight / 2;
-    ctx.drawImage(sun, sunX, sunY, sunWidth, sunHeight);
-
-    canvas.addEventListener("mousemove", function (event) {
-      const mouseX = event.clientX - canvas.getBoundingClientRect().left;
-      const mouseY = event.clientY - canvas.getBoundingClientRect().top;
-
-      const angle = Math.atan2(mouseY - center.y, mouseX - center.x); // Adjust the angle
-
-      const distance = 130;
-      const shuttleX =
-        center.x + distance * Math.cos(angle) - shuttle.width / 6;
-      const shuttleY =
-        center.y + distance * Math.sin(angle) - shuttle.height / 6;
-
-      ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
-
-      // Draw the sun at the center with width and height as 100px
-      ctx.drawImage(sun, sunX, sunY, sunWidth, sunHeight);
-      ctx.drawImage(quesBox, 700, sunY + 30, sunWidth / 1.2, sunHeight / 1.2);
-      ctx.fillStyle = "white";
-      quesText.style.display = "block";
-
-      ctx.save();
-      ctx.translate(
-        shuttleX + shuttle.width / 6,
-        shuttleY + shuttle.height / 6
-      );
-      ctx.rotate(angle);
-      ctx.drawImage(
-        shuttle,
-        -shuttle.width / 6,
-        -shuttle.height / 6,
-        shuttle.width / 3,
-        shuttle.height / 3
-      );
-      ctx.restore();
-    });
-  };
+  canvas.addEventListener("mousemove", function (event) {
+    mouseX = event.clientX - canvas.getBoundingClientRect().left;
+    mouseY = event.clientY - canvas.getBoundingClientRect().top;
+  });
+  setInterval(generateAsteroid, 3000);
+  gameLoop();
 }
 
-// Call Game Function when Start button is clicked
-document.getElementById("startButton").addEventListener("click", startGame);
+startbtn.addEventListener("click", startGame);
